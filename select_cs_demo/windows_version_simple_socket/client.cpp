6e6c7ea04169e1befe55d2f4965c6aca69a4187e
albert <alberts101@163.com>
2018-07-05 20:42:03 +0800

    update simple socket example on windows

--- select_cs_demo/windows_version_simple_socket/client.cpp
+++ select_cs_demo/windows_version_simple_socket/client.cpp
@@ -5,32 +5,32 @@
 
 int main(int argc, char *argv[])
 {
-	WSADATA wsaData;
-	WORD wVersionRequested = MAKEWORD(2, 2);
-	WSAStartup(wVersionRequested, &wsaData);
-	int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    WSADATA wsaData;
+    WORD wVersionRequested = MAKEWORD(2, 2);
+    WSAStartup(wVersionRequested, &wsaData);
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-	struct sockaddr_in server_addr;
-	memset(&server_addr, 0, sizeof(server_addr));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(8899);
-	server_addr.sin_addr.s_addr	= inet_addr("127.0.0.1");
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family      = AF_INET;
+    server_addr.sin_port        = htons(8899);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-	connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
-	while(1)
-	{
-		char szSendBuf[64] = {0};
-		for(unsigned int i = 0; i < 64; i++)
-		{
-			szSendBuf[i] = 'a';	
-		}
+    while(1)
+    {
+        char szSendBuf[64] = {0};
+        for(unsigned int i = 0; i < 64; i++)
+        {
+            szSendBuf[i] = 'a';    
+        }
 
-		int iRet = send(client_fd, szSendBuf, sizeof(szSendBuf) , 0); 
-		printf("send size is %d, iRet is %d\n", sizeof(szSendBuf), iRet);
-		getchar();
-	}
+        int iRet = send(client_fd, szSendBuf, sizeof(szSendBuf) , 0); 
+        printf("send size is %d, iRet is %d\n", sizeof(szSendBuf), iRet);
+        getchar();
+    }
 
-	closesocket(client_fd);
-	return 0;
+    closesocket(client_fd);
+    return 0;
 }
--- select_cs_demo/windows_version_simple_socket/server.cpp
+++ select_cs_demo/windows_version_simple_socket/server.cpp
@@ -5,35 +5,35 @@
 
 int main(int argc, char * argv[])
 {
-	WSADATA wsaData;
-	WORD wVersionRequested = MAKEWORD(2, 2);
-	WSAStartup(wVersionRequested, &wsaData);
-	int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    WSADATA wsaData;
+    WORD wVersionRequested = MAKEWORD(2, 2);
+    WSAStartup(wVersionRequested, &wsaData);
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-	struct sockaddr_in server_addr;
-	memset(&server_addr, 0, sizeof(server_addr));
-	server_addr.sin_family		= AF_INET;
-	server_addr.sin_port		= htons(8899);
-	server_addr.sin_addr.s_addr	= INADDR_ANY;
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family      = AF_INET;
+    server_addr.sin_port        = htons(8899);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
-	bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-	listen(listen_fd, 10);
+    bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    listen(listen_fd, 10);
 
-	printf("<server>server[ip:%s, port:%d] is running...\n", "127.0.0.1", 8899);
+    printf("<server>server[ip:%s, port:%d] is running...\n", "127.0.0.1", 8899);
 
-	int len = sizeof(server_addr);
-	int client_id = accept(listen_fd, (struct sockaddr *)&server_addr, &len);
+    int len = sizeof(server_addr);
+    int client_id = accept(listen_fd, (struct sockaddr *)&server_addr, &len);
 
-	while(1)  
-	{  
-		char szRecvBuf[50001] = {0};  
-		int iRet = recv(client_id, szRecvBuf, sizeof(szRecvBuf)-1, 0);  
-		printf("iRet is %d\n", iRet);	
+    while(1)  
+    {
+        char szRecvBuf[50001] = {0};  
+        int iRet = recv(client_id, szRecvBuf, sizeof(szRecvBuf)-1, 0);  
+        printf("iRet is %d\n", iRet);    
 
-		getchar();	
-		closesocket(client_id);
-	} 
-	
-	closesocket(listen_fd);
-	return 0;
+        getchar();    
+        closesocket(client_id);
+    }
+    
+    closesocket(listen_fd);
+    return 0;
 }
